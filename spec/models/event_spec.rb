8322be3f6874dedc48da393a237e490a62334b3a
praz <t.prazwal@gmail.com>
2020-08-25 12:50:20 +0200

    add tests for model associations

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,31 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  subject {
-    described_class.new(
-      title: 'Hello',
-      description: 'World',
-      events_date: DateTime.now + 1.week,
-      creator: User.new
-    )
-  }
+  context 'ActiveModel Validations' do
+    subject {
+      described_class.new(
+        title: 'Hello',
+        description: 'World',
+        events_date: DateTime.now + 1.week,
+        creator: User.new
+      )
+    }
 
-  it 'is valid with valid attirbutes' do
-    expect(subject).to be_valid
+    it 'is valid with valid attirbutes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without events_date' do
+      subject.events_date = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without title' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without description' do
+      subject.description = nil
+      expect(subject).to_not be_valid
+    end
   end
 
-  it 'is not valid without events_date' do
-    subject.events_date = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid without title' do
-    subject.title = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid without description' do
-    subject.description = nil
-    expect(subject).to_not be_valid
+  context 'ActiveRecord Associations' do
+    it { expect(described_class.reflect_on_association(:event_attendings).macro).to eq(:has_many) }
+    it { expect(described_class.reflect_on_association(:attendees).macro).to eq(:has_many) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,32 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject {
-    described_class.new(
-      name: 'Prazwal',
-      username: 'praz'
-    )
-  }
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
-  it 'is not valid without a name' do
-    subject.name = nil
-    expect(subject).to_not be_valid
+  context 'ActiveModel Validations' do
+    subject {
+      described_class.new(
+        name: 'Prazwal',
+        username: 'praz'
+      )
+    }
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+    it 'is not valid without a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid with name less than 3 characters' do
+      subject.name = 'pr'
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without username' do
+      subject.username = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid with username less than 3 characters' do
+      subject.username = 'pr'
+      expect(subject).to_not be_valid
+    end
   end
 
-  it 'is not valid with name less than 3 characters' do
-    subject.name = 'pr'
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid without username' do
-    subject.username = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid with username less than 3 characters' do
-    subject.username = 'pr'
-    expect(subject).to_not be_valid
+  context 'ActiveRecord Associations' do
+    it { expect(described_class.reflect_on_association(:event_attendings).macro).to eq(:has_many) }
+    it { expect(described_class.reflect_on_association(:attended_events).macro).to eq(:has_many) }
   end
 end
