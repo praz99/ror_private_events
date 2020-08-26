cf09a3c946117a67d4e25c2dbc679cdbc8fd0e21
okikiola11 <okikiola.apelehin@gmail.com>
2020-08-26 16:29:37 +0000

    Add the feature test for authentication

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name { 'kiki' }
     username { 'kikiolla' }
+    name { 'kikiola' }
   end
 end
--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.feature 'the signup process' do
   before(:example) do 
@@ -20,10 +21,47 @@ RSpec.feature 'signing up a user' do
   end
 
   scenario 'after signing up redirect to user SHOW page' do
-    expect(page).to have_content ' '
+    expect(page).to have_content 'Display all users'
   end
 
   scenario 'display username ontop of the page after signing up' do
     expect(page).to have_content 'kikiolla'
+  end
+end
+
+RSpec.feature 'signing in as a non-existing user' do
+  before(:example) do
+    visit login_path
+    fill_in 'username', with: 'ccccccc'
+    first(:link, 'Log in').click
+  end
+
+  scenario 'should not redirect to user page' do 
+    expect(page).not_to have_content ' Hello world '
+  end
+
+  scenario 'should display error message if user does not exist' do
+    expect(page).to_not have_content 'Invalid username'
+  end
+end
+
+RSpec.feature 'should sign in an existing user' do 
+  before(:example) do
+    FactoryBot.create(:user)
+    visit '/login'
+    fill_in 'session[username]', with: 'kikiolla'
+    first(:link, 'Log in').click
+  end
+
+  scenario 'should redirect to user page after signup' do
+    expect(page).to have_content ' '
+  end
+
+  scenario 'shows username on the homepage after signup' do
+    expect(page).to have_content ''
+  end
+
+  scenario 'should not dispaly an error that the user does not exist' do
+    expect(page).not_to have_content 'User does not exist'
   end
 end
