3b959b3a9e1823a024358787aa9022d853253471
praz <t.prazwal@gmail.com>
2020-08-28 11:49:27 +0200

    generate new migration add index for events table in creator_id column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_152446) do
+ActiveRecord::Schema.define(version: 2020_08_28_094502) do
 
   create_table "event_attendings", force: :cascade do |t|
     t.integer "attended_events_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_08_17_152446) do
     t.integer "creator_id"
     t.string "title"
     t.text "description"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
