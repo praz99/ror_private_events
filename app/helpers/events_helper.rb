07e4749af92adbc0e70ad8a4a5513e361d47fcdd
praz <t.prazwal@gmail.com>
2020-08-28 12:55:37 +0200

    Remove logic from views create different partials for each logic condition, render the partials from methods within view helpers.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,15 @@
 module EventsHelper
+  def session_check
+    if logged_in?
+      render 'helper_partials/logged_in'
+    else
+      render 'helper_partials/not_logged_in'
+    end
+  end
+
+  def errors_check
+    if @user.errors.any?
+      render 'helper_partials/errors_count'
+    end
+  end
 end
